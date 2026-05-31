6987877122ef52171c1e3ebd44ab8c2680bb0003
Peter Baus <peter.baus@accenture.com>
2026-05-31 16:11:15 +0200

    Update base path

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/prompt-library/dist/sw.js', { scope: '/prompt-library/dist/' })})}
